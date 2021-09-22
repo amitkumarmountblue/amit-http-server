25337f5bbaca8345a6a9fc33190695e5d84beec2
amitkumarmountblue <amit.kumar.17@mountblue.tech>
2021-09-22 18:00:37 +0530

    express

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const app = express();
 const { v4: uuidv4 } = require("uuid");
 
 app.get("/html", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res
+     .status(200)
+     .sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/json", (req, res) => {
-  res.sendFile(path.join(__dirname, "data.json"));
+  res.status(200).sendFile(path.join(__dirname, "data.json"));
 });
 app.get("/status/:statusCode", (req, res) => {
   const statusCode = req.params.statusCode;
-  res.status(statusCode);
-  res.send(`${statusCode}`);
+  res.status(statusCode).send(`${statusCode}`);
 });
 app.get("/uuid", (req, res) => {
   let UUID = { data: uuidv4() };
-  res.send(UUID);
+  res.status(200).send(UUID);
 });
 app.get("/delay/:delayTime", (req, res) => {
   const delay = req.params.delayTime;
   setTimeout(() => {
-    res.send(`response successful with the delay of ${delay}s`);
-  }, delay * 1000);
+    res.status(200).send(`response successful with the delay of ${delay}s`);
+  }, parseInt(delay) * 1000);
 });
 
 app.listen(8080, () => {
